f5d9871575d3b356e737bc0854b2cc00594a818f
Christian Siewert <c067605@gmail.com>
2026-02-10 20:48:05 +0100

    implemented on demand grid structure building

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,8 +9,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Added
 
 - layout structure for main panel grid system
-
-### Changed
+- on demand grid structure building
 
 ## [0.0.1] - 2026-02-10
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,60 @@
+/**
+ * Application JavaScript code for Concise view
+ * 
+ */
 var Concise = function() {
+
+    /**
+     * Registers all event listeners we need
+     */
     var registerEvents = function() {
+
+        /**
+         * Adds a new RSS source column to the grid when the "Add RSS Source" button is clicked.
+         */
         $('#add_rss_source').click(function(e){
-            swal({
-                title: 'Input Something',
-                content: {
-                    element: "input",
-                    attributes: {
-                        placeholder: "Input Something",
-                        type: "text",
-                        id: "input-field",
-                        className: "form-control"
-                    },
-                },
-                buttons: {
-                    cancel: {
-                        visible: true,
-                        className: 'btn btn-danger'
-                    },        			
-                    confirm: {
-                        className : 'btn btn-secondary'
-                    }
-                },
-            }).then(
-            function() {
-                console.log($('#input-field').val())
-            }
-            );
+
+            const row1 = document.getElementById("row1");
+            const row2 = document.getElementById("row2");
+
+            /**
+             * Creates a new DomNode and appends it to the corresponding row
+             * 
+             * @returns {HTMLDivElement}
+             */
+            const createColumn = () => {
+
+                const col = document.createElement("div");
+                const tpl = document.getElementById("column_template"); 
+                const clone = tpl.content.cloneNode(true); 
+
+                col.classList.add("col-2", "h-100");
+                col.appendChild(clone);
+
+                // Initialize scrollbar as soon as the element is in the DOM 
+                setTimeout(() => { 
+                    $(col).find(".scrollbar-inner").scrollbar();
+                }, 0);
+
+                return col;
+            };
+
+            /**
+             * For now we are using 6 columns and 2 rows as maximum
+             */
+            if (row1.children.length < 6) return row1.appendChild(createColumn());
+            if (row2.children.length < 6) return row2.appendChild(createColumn());
         });
     };
 
+    // Init scrollbars when page is loaded
     var initScrollbars = function() {    
         $('.scrollbar-inner').scrollbar();
     };
 
+    /**
+     * Public API of the Concise view module
+     */
     return {
         init: function() {
             registerEvents();
@@ -41,6 +63,9 @@ var Concise = function() {
     };
 }();
 
+/**
+ * Initialize the Concise view when the document is ready
+ */
 jQuery(document).ready(function() {
     Concise.init();
 });
